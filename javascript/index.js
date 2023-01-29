679d81d4b18011eff7a69bf463481b028f56d4c6
amelie <ameliegitti@gmail.com>
2023-01-29 16:00:18 +0000

    Added the js file for 2 locations

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,22 @@
+function updateTime() {
+  let chicagoElement = document.querySelector("#chicago");
+  let chicagoDateElement = chicagoElement.querySelector(".date");
+  let chicagoTimeElement = chicagoElement.querySelector(".time");
+  let chicagoTime = moment().tz("America/Chicago");
 
+  chicagoDateElement.innerHTML = chicagoTime.format("MMMM Do YYYY");
+  chicagoTimeElement.innerHTML = chicagoTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  let milanElement = document.querySelector("#milan");
+  let milanDateElement = milanElement.querySelector(".date");
+  let milanTimeElement = milanElement.querySelector(".time");
+  let milanTime = moment().tz("Europe/Rome");
+
+  milanDateElement.innerHTML = milanTime.format("MMMM Do YYYY");
+  milanTimeElement.innerHTML = milanTime.format("h:mm:ss [<small>]A[</small>]");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
