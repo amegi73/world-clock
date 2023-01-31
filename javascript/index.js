4a4566c67023af5be26cd4e7e051b91faf954b8b
amelie <ameliegitti@gmail.com>
2023-01-31 11:22:34 +0000

    Added the functionality for the select

--- javascript/index.js
+++ javascript/index.js
@@ -1,22 +1,48 @@
 function updateTime() {
   let chicagoElement = document.querySelector("#chicago");
-  let chicagoDateElement = chicagoElement.querySelector(".date");
-  let chicagoTimeElement = chicagoElement.querySelector(".time");
-  let chicagoTime = moment().tz("America/Chicago");
+  if (chicagoElement) {
+    let chicagoDateElement = chicagoElement.querySelector(".date");
+    let chicagoTimeElement = chicagoElement.querySelector(".time");
+    let chicagoTime = moment().tz("America/Chicago");
 
-  chicagoDateElement.innerHTML = chicagoTime.format("MMMM Do YYYY");
-  chicagoTimeElement.innerHTML = chicagoTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    chicagoDateElement.innerHTML = chicagoTime.format("MMMM Do YYYY");
+    chicagoTimeElement.innerHTML = chicagoTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   let milanElement = document.querySelector("#milan");
-  let milanDateElement = milanElement.querySelector(".date");
-  let milanTimeElement = milanElement.querySelector(".time");
-  let milanTime = moment().tz("Europe/Rome");
+  if (milanElement) {
+    let milanDateElement = milanElement.querySelector(".date");
+    let milanTimeElement = milanElement.querySelector(".time");
+    let milanTime = moment().tz("Europe/Rome");
 
-  milanDateElement.innerHTML = milanTime.format("MMMM Do YYYY");
-  milanTimeElement.innerHTML = milanTime.format("h:mm:ss [<small>]A[</small>]");
+    milanDateElement.innerHTML = milanTime.format("MMMM Do YYYY");
+    milanTimeElement.innerHTML = milanTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+}
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = ` 
+  <div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format(
+            "h:mm:ss"
+          )} <small>${cityTime.format("A")} </small></div>
+        </div>`;
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
